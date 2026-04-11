dc089ce65b86ebc35fe8bacc77efbea819cda85e
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-04-11 22:16:24 +0100

    Add optimal solution

--- 1_Arrays_and_Hashing/2_Valid_Anagram/solution.py
+++ 1_Arrays_and_Hashing/2_Valid_Anagram/solution.py
@@ -1,6 +1,21 @@
 class Solution:
     def is_anagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+        if len(s) != len(t):
+            return False
+        
+        # Assuming all letters are in lowercase
+        track_letters = [0] * 26
+        
+        # Both strings are equal so they hjave the same lengths
+        for i in range(len(s)):
+            track_letters[ ord(s[i]) - ord('a') ] += 1
+            track_letters[ ord(t[i]) - ord('a') ] -= 1
+
+        for val in track_letters:
+            if val != 0:
+                # i.e if the value does not reset, then it is not p[resent in the two strings
+                return False
+        return True
 
 if __name__ == "__main__":
     solution = Solution()

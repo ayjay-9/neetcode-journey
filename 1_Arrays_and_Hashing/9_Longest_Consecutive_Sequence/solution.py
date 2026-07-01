22d1f28627bc0af7d1a123f53890a017d062cc1e
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-07-01 15:53:27 +0100

    Add solution for longest consecutive sequence

---
+++ 1_Arrays_and_Hashing/9_Longest_Consecutive_Sequence/solution.py
@@ -0,0 +1,27 @@
+class Solution:
+    def longestConsecutive(self, nums: list[int]) -> int:
+        ordered_nums = sorted(set(nums))
+        count = 1
+        sequence = []
+
+        # Edge Case 1: [], i.e no elements in the list
+        if not ordered_nums:
+            return 0
+
+        # Edge Case 2: [0] or [1], i.e 1 element in the sorted set
+        if len(ordered_nums) == 1:
+            return count
+
+        for i in range(len(ordered_nums) - 1):
+            if (ordered_nums[i + 1] == ordered_nums[i] + 1):
+                count = count + 1
+            else:
+                sequence.append(count)
+                count = 1
+                continue
+            sequence.append(count) # If everything is in sequence
+        return max(sequence)
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.longestConsecutive([0, 0]))

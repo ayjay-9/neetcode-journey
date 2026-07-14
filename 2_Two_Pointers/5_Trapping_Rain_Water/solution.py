c10097a0fb33bb34cc793a9bb20f7eb6c762d246
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-07-14 12:53:28 +0100

    add solution for trapping rain water

--- 2_Two_Pointers/5_Trapping_Rain_Water/solution.py
+++ 2_Two_Pointers/5_Trapping_Rain_Water/solution.py
@@ -1,29 +1,26 @@
 class Solution:
     def trap(self, height: list[int]) -> int:
-        # Distance between l_pointer and r_pointer should be at least 2
-        l_index = 0
-        r_index = l_index+2
-        max_vol, vol, temp = 0, 0, 0
+        if len(height) < 3:
+            return 0
 
-        # if r_height >= l_height, that is the total volume... Move both pointers
-        # else move the right pointer 1 place
-        for i in range(r_index-1, len(height)-1):
-            l_pointer, r_pointer = height[l_index], height[r_index]
-            if l_pointer <= r_pointer:
-                vol = min(l_pointer, r_pointer) - height[i]
+        left = 0
+        right = len(height) - 1
+        left_max = height[left]
+        right_max = height[right]
+        water = 0
+
+        while left < right:
+            if left_max <= right_max:
+                left += 1
+                left_max = max(left_max, height[left])
+                water += left_max - height[left]
             else:
-                vol = l_pointer - height[i]
-            if vol > 0:
-                temp += vol
-            if r_pointer >= l_pointer:
-                max_vol += temp # don't update/reset unless there is an equal or higher height
-                temp = 0
-                l_index = r_index - 1
-                r_index = l_index+2
-            else:
-                r_index += 1
-        return max_vol
+                right -= 1
+                right_max = max(right_max, height[right])
+                water += right_max - height[right]
+
+        return water
 
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.trap([0,1,0,2,1,0,1,3,2,1,2,1]))
+    print(solution.trap([0,2,0,3,1,0,1,3,2,1]))

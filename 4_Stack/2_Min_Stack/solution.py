9bef12c77601bad609049af3d4189768d62c950f
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-08-07 14:13:58 +0100

    reformat file

---
+++ 4_Stack/2_Min_Stack/solution.py
@@ -0,0 +1,26 @@
+class MinStack:
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return min(self.stack)
+
+
+if __name__ == "__main__":
+    minStack = MinStack()
+    minStack.push(-1)
+    minStack.push(1)
+    minStack.push(-1)
+    print(minStack.getMin())
+    minStack.pop()
+    print(minStack.top())
+    print(minStack.getMin())

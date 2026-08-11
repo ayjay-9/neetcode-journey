77b7742f47307093216f30cb9a9e2eef04506cfb
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-08-11 12:45:08 +0100

    add solution for largest rectangle in histogram

---
+++ 4_Stack/6_Largest_Rectangle_in_Histogram/solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def largestRectangleArea(self, heights: list[int]) -> int:
+        max_area = 0
+        stack = []  # stores pairs: (start_index, height)
+
+        for i, height in enumerate(heights):
+            start = i
+            while stack and stack[-1][1] > height:
+                index, popped_height = stack.pop()
+                max_area = max(max_area, popped_height * (i - index))
+                start = index
+            stack.append((start, height))
+
+        for index, height in stack:
+            max_area = max(max_area, height * (len(heights) - index))
+        return max_area
+
+if __name__ == "__main__":
+    solution = Solution()
+    print(solution.largestRectangleArea(heights=[7,1,7,2,2,4]))

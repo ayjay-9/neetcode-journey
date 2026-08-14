ac267fabeddf97381ebfe5a6b902a90c205a2734
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-08-14 16:34:16 +0100

    add binary search solution

--- 5_Binary_Search/5_Search_in_Rotated_Sorted_Array/solution.py
+++ 5_Binary_Search/5_Search_in_Rotated_Sorted_Array/solution.py
@@ -3,11 +3,25 @@ from operator import index
 
 class Solution:
     def search(self, nums: list[int], target: int) -> int:
-        if target in nums:
-            return nums.index(target)
-        else:
-            return -1
+        start, end = 0, len(nums)-1
+
+        while start <= end:
+            mid = (start+end) // 2
+            if target == nums[mid]:
+                return mid
+            # Left sorted portion
+            if nums[start] <= nums[mid]:
+                if target > nums[mid] or target < nums[start]: # Go to right portion
+                    start = mid+1
+                else: # Eliminate right portion
+                    end = mid-1
+            else: # Right sorted portion
+                if target < nums[mid] or target > nums[end]: # Go to left portion
+                    end = mid-1
+                else:
+                    start = mid+1
+        return -1
 
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.search(nums=[3,5,6,0,1,2], target=6))
+    print(solution.search(nums=[3,4,5,6,1,2], target=1))

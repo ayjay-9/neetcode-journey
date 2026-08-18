3ab360fe4980afae883fa1e371b95bf04bb53022
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-08-18 14:46:12 +0100

    fix: TLE

--- 5_Binary_Search/6_Time_Based_Key_Value_Store/solution.py
+++ 5_Binary_Search/6_Time_Based_Key_Value_Store/solution.py
@@ -11,25 +11,24 @@ class TimeMap:
         self.map[key].append([value, timestamp])
 
     def get(self, key: str, timestamp: int) -> str:
-        copy = sorted(self.map[key], key=lambda item: item[1])
-        start, end = 0, len(copy)-1
+        start, end = 0, len(self.map[key])-1
         max_time, answer = float("inf"), ""
         while start <= end:
             mid = (start + end) // 2
-            timestamp_prev = copy[mid][1]
+            timestamp_prev = self.map[key][mid][1]
             if timestamp < timestamp_prev:
                 end = mid-1
             else:
                 start = mid+1
             if timestamp_prev <= timestamp:
-                answer = copy[mid][0]
+                answer = self.map[key][mid][0]
         return answer if answer else ""
 
 if __name__ == "__main__":
     timeMap = TimeMap()
     timeMap.set("alice", "happy", 1)
-    timeMap.set("alice", "sad", 10)
+    timeMap.set("alice", "sad", 3)
     timeMap.set("alice", "happy", 5)
     timeMap.set("alice", "sad", 7)
-    timeMap.set("alice", "sad", 3)
-    print(timeMap.get("alice", 0))
+    timeMap.set("alice", "sad", 10)
+    print(timeMap.get("alice", 1))

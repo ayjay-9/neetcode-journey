446915676d0452eb5bb61df18fd9ef446f00dfee
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-08-19 15:35:14 +0100

    improve time complexity from 48ms(beats6.30%) to 28ms(beats 79.3%)

--- 5_Binary_Search/7_Median_of_Two_Sorted_Arrays/solution.py
+++ 5_Binary_Search/7_Median_of_Two_Sorted_Arrays/solution.py
@@ -4,15 +4,14 @@ from math import floor, ceil
 class Solution:
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
         # Median = ceil((N+1) / 2)th position
-        combined_array = nums1
-        combined_array += [num for num in nums2]
-        combined_array = sorted(combined_array)
-        l, r = 0, len(combined_array)-1
+        nums1.extend(nums2)
+        nums1 = sorted(nums1)
+        l, r = 0, len(nums1)-1
         size, pos, median = r+1, (l+r) / 2, 0
         if pos.is_integer(): # Median pos is an integer number
-            median = combined_array[int(pos)]
+            median = nums1[int(pos)]
         else: # Median pos is a float, median is the average of the two middle numbers
-            median = (combined_array[floor(pos)] + combined_array[ceil(pos)]) / 2
+            median = (nums1[floor(pos)] + nums1[ceil(pos)]) / 2
         return median
 
 if __name__ == "__main__":

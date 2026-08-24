40c93f0c537d588cd214487a0940a65b36b8293f
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-08-24 12:04:13 +0100

    add solution for remove nth node from end of list

--- 6_Linked_List/5_Remove_Nth_Node_From_End_of_List/solution.py
+++ 6_Linked_List/5_Remove_Nth_Node_From_End_of_List/solution.py
@@ -6,17 +6,20 @@ class ListNode:
 
 class Solution:
     def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
-        prev, slow, nxt, index = None, head, head,  0
-        while slow and slow.next:
-            nxt = nxt.next
-            if index == n: # Update the pointers
-                prev.next = nxt
-                slow.next = None # Break the link at the node to be removed
-                break
-            prev = slow
+        # e.g Return the 2nd Node from the end of the list, not 2nd index
+        dummy = ListNode(0, head) # dummy(0) -> 1 -> 2 -> 3 -> None
+        fast = dummy
+        slow = dummy
+        for _ in range(n): # If n = 2 fast stops at 2, dummy(0) -> 1 -> 2(fast) -> 3 -> None
+            fast = fast.next
+
+        while fast.next: # dummy(0) -> 1(slow) -> 2 -> 3(fast) -> None
+            fast = fast.next
             slow = slow.next
-            index += 1
-        return head
+
+        slow.next = slow.next.next # 1.next == 2, so it now becomes 1.next == 2.next
+        # dummy(0) -> 1(slow) -> 3(fast) -> None
+        return dummy.next # 1(slow) -> 3(fast) -> None
 
 def build_linked_list(values):
     head = None
@@ -40,6 +43,6 @@ def print_list(head) -> list:
 
 if __name__ == "__main__":
     solution = Solution()
-    head1 = build_linked_list([5])
+    head1 = build_linked_list([1, 2, 3])
     print(print_list(head1))
-    print(print_list(solution.removeNthFromEnd(head=head1, n=1)))
+    print(print_list(solution.removeNthFromEnd(head=head1, n=2)))

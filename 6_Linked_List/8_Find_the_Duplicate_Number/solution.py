f08283dbd034df435773a2955ae098abf4803312
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-08-25 13:04:08 +0100

    add solution for find duplicate using Floyd's algorithm, improved time complexity from 14.33% to 33.07%

--- 6_Linked_List/8_Find_the_Duplicate_Number/solution.py
+++ 6_Linked_List/8_Find_the_Duplicate_Number/solution.py
@@ -1,13 +1,20 @@
 class Solution:
     def findDuplicate(self, nums: list[int]) -> int:
-        nums = sorted(nums)
-        l, r = 0, 1
-        while l <= r:
-            if nums[l] != nums[r]:
-                l += 1
-                r += 1
-            else:
-                return nums[l]
+        # Floyd's Algorithm to detect cycles
+        slow, fast = 0, 0
+        while True:
+            slow = nums[slow]
+            index = nums[fast]
+            fast = nums[index]
+            if slow == fast: # First intersection
+                break
+
+        slow2 = 0
+        while True:
+            slow = nums[slow]
+            slow2 = nums[slow2]
+            if slow == slow2: # Index of start of loop
+                return slow or slow2
 
 if __name__ == "__main__":
     solution = Solution()

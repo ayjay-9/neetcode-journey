30afade3dac3359f206852b602f7879a6754e23c
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-08-26 16:03:09 +0100

    add optimal solution for LRU Cache

--- 6_Linked_List/9_LRU_Cache/solution.py
+++ 6_Linked_List/9_LRU_Cache/solution.py
@@ -1,19 +1,18 @@
+from collections import OrderedDict
+
+
 class LRUCache:
 
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.map = {}
-        self.rank = 0 # Track the age
+        self.map = OrderedDict()
 
     def get(self, key: int) -> int:
-        if key in self.map:
-            # Increase the rank to be the highest rank
-            max_rank = max(self.map.items(), key=lambda item: item[1][0])[1][0]
-            self.map[key][0] = max_rank+1 # Update the rank
-            self.rank = max_rank+2
-            return self.map[key][1]
-        else:
+        if key not in self.map:
             return -1
+
+        self.map.move_to_end(key)
+        return self.map[key]
 
     def put(self, key: int, value: int) -> None:
         # 1. Update the key's value if it exists
@@ -21,14 +20,15 @@ class LRUCache:
         # 3. Remove the last used key
         # put() can only do either of the three
 
-        if key in self.map or len(self.map) < self.capacity:
-            self.map[key] = [self.rank, value]
-            self.rank += 1
-        else: # Remove the key with the lowest rank -> sort[0][0]
-            min_rank_key = min(self.map.items(), key=lambda item: item[1][0])[0]
-            del self.map[min_rank_key]
-            self.map[key] = [self.rank, value]
-            self.rank += 1
+        if key in self.map:
+            self.map[key] = value
+            self.map.move_to_end(key)
+            return
+
+        if len(self.map) == self.capacity:
+            self.map.popitem(last=False)
+
+        self.map[key] = value
 
 if __name__ == "__main__":
     lru_cache = LRUCache(3)

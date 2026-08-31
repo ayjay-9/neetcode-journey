8a2a0a508fcd445c814cb47a45f8288899fee103
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-08-31 13:02:33 +0100

    add iterative DFS solution for max depth of binary tree

--- 7_Trees/2_Maximum_Depth_of_Binary_Tree/solution.py
+++ 7_Trees/2_Maximum_Depth_of_Binary_Tree/solution.py
@@ -18,20 +18,32 @@ class Solution:
         # return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
         #BFS
-        if not root:
-            return 0
+        # if not root:
+        #     return 0
+        #
+        # level = 0
+        # q = deque([root])
+        # while q:
+        #     for _ in range(len(q)):
+        #         node = q.popleft()
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        #     level += 1
+        # return level
 
-        level = 0
-        q = deque([root])
-        while q:
-            for _ in range(len(q)):
-                node = q.popleft()
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-            level += 1
-        return level
+        # Iterative Pre-order DFS
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res,depth)
+                stack.append([node.left, depth+1])
+                stack.append([node.right, depth+1])
+        return res
 
 def build_tree(values):
     if not values:

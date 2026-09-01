5325b752ce0172f2409d0205a2e302950ba9defe
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-09-01 13:54:39 +0100

    add solution for diameter of a binary tree

---
+++ 7_Trees/3_Diameter_of_a_Binary_Tree/solution.py
@@ -0,0 +1,65 @@
+# Definition for a binary tree node.
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def diameterOfBinaryTree(self, root: TreeNode) -> int:
+        # Diameter = Longest sequence of edges
+        self.diameter = 0
+        def dfs(curr: TreeNode) -> int:
+            if not curr:
+                return 0
+            left, right = dfs(curr.left), dfs(curr.right)
+            self.diameter = max(self.diameter, left+right)
+            return 1 + max(left, right)
+        dfs(root)
+        return self.diameter
+
+def build_tree(values):
+    if not values:
+        return None
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+    while queue and i < len(values):
+        current = queue.popleft()
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+
+def print_tree(root):
+    if not root:
+        return []
+    result = []
+    queue = deque([root])
+    while queue:
+        current = queue.popleft()
+        if current:
+            result.append(current.val)
+            queue.append(current.left)
+            queue.append(current.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    return result
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    root = build_tree([1,None,2,3,4,5])
+    print(print_tree(root))
+    print(solution.diameterOfBinaryTree(root))

5aa67e64a2ca053fdfa06ef8150d5202e9866c3a
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-09-11 00:21:58 +0100

    add solution for building binary tree from preorder and postorder lists

---
+++ 7_Trees/13_Construct_Binary_Tree_From_Preorder_and_Inorder_Traveral/solution.py
@@ -0,0 +1,74 @@
+# Definition for a binary tree node.
+from collections import deque
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def buildTree(self, preorder: list[int], inorder: list[int]) -> TreeNode:
+        # preorder, inorder = [1,2,3,4], [2,1,3,4]
+        if not inorder or not preorder:
+            return None
+        inorder_index = {val: i for i, val in enumerate(inorder)}
+        self.pre_idx = 0
+
+        def build(left: int, right: int) -> TreeNode:
+            if left > right:
+                return None
+
+            root_val = preorder[self.pre_idx]
+            self.pre_idx += 1
+            root = TreeNode(root_val)
+
+            mid = inorder_index[root_val]
+            root.left = build(left, mid - 1)
+            root.right = build(mid + 1, right)
+            return root
+
+        return build(0, len(inorder) - 1)
+
+def build_tree(values):
+    if not values:
+        return None
+    root = TreeNode(values[0])
+    queue = deque([root])
+    i = 1
+    while queue and i < len(values):
+        current = queue.popleft()
+        if i < len(values) and values[i] is not None:
+            current.left = TreeNode(values[i])
+            queue.append(current.left)
+        i += 1
+        if i < len(values) and values[i] is not None:
+            current.right = TreeNode(values[i])
+            queue.append(current.right)
+        i += 1
+    return root
+
+
+def print_tree(root):
+    if not root:
+        return []
+    result = []
+    queue = deque([root])
+    while queue:
+        current = queue.popleft()
+        if current:
+            result.append(current.val)
+            queue.append(current.left)
+            queue.append(current.right)
+        else:
+            result.append(None)
+    while result and result[-1] is None:
+        result.pop()
+    return result
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    preorder, inorder = [1,2,3,4], [2,1,3,4]
+    print(f"Prorder:{preorder}, Inorder: {inorder} = {print_tree(solution.buildTree(preorder=preorder, inorder=inorder))}")

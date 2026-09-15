27433fdc88b2f33ade6ff381fef3a1dc27373333
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-09-15 14:17:02 +0100

    update to bitwise solution, previously counter to improve time complexity

--- 18_Bit_Manipulation/1_Single_Number/solution.py
+++ 18_Bit_Manipulation/1_Single_Number/solution.py
@@ -3,9 +3,10 @@ from collections import Counter
 
 class Solution:
     def singleNumber(self, nums: list[int]) -> int:
-        freq = Counter(nums)
-        freq = sorted(freq.items(), key=lambda item: item[1])
-        return freq[0][0]
+        ans = 0
+        for num in nums:
+            ans ^= num
+        return ans
 
 if __name__ == "__main__":
     solution = Solution()

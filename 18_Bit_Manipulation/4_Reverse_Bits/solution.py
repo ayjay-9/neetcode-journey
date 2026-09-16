017b0f9979aa41e72d7752622fb0f01c66209fde
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-09-16 14:40:09 +0100

    add simplified solution for reversing bits of an unsigned integer

--- 18_Bit_Manipulation/4_Reverse_Bits/solution.py
+++ 18_Bit_Manipulation/4_Reverse_Bits/solution.py
@@ -2,8 +2,8 @@ class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
         for i in range(32):
-            bit = (n >> i) & 1
-            res |= bit << (31-i)
+            res = (res << 1) | (n&1)
+            n >>= 1
         return bin(res)
 
 if __name__ == "__main__":

4ebad4de0fa40d702e829e0540bb93125f2a9dc6
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-09-16 13:58:26 +0100

    use bit shifting to count number of 1's

--- 18_Bit_Manipulation/2_Number_of_1_Bits/solution.py
+++ 18_Bit_Manipulation/2_Number_of_1_Bits/solution.py
@@ -1,10 +1,9 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
-        binary = str(format(n, "b"))
         count = 0
-        for bit in binary:
-            if int(bit) ^ 1 == 0:
-                count += 1
+        while n:
+            count += n%2
+            n = n >> 1
         return count
 
 if __name__ == "__main__":

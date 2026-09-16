e490d7a59c0eb0673345abd5636b09eb18b0e6ea
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-09-16 14:07:34 +0100

    improve space compleity for counting bits

--- 18_Bit_Manipulation/3_Counting_Bits/sokution.py
+++ 18_Bit_Manipulation/3_Counting_Bits/sokution.py
@@ -2,11 +2,7 @@ class Solution:
     def countBits(self, n: int) -> list[int]:
         res = []
         for bit in range(n+1):
-            count = 0
-            while bit:
-                bit &= bit-1
-                count += 1
-            res.append(count)
+            res.append(bin(bit).count('1')) # O(1) extra space
         return res
 
 if __name__ == "__main__":

3a7a81c0e08a1f0878bf64d421126e3201c584bc
ayjay-9 <ayobanjoemmanuel1@gmail.com>
2026-09-17 13:30:33 +0100

    add solution for adding numbers (Python)

--- 18_Bit_Manipulation/6_Sum_of_Two_Integers/solution.py
+++ 18_Bit_Manipulation/6_Sum_of_Two_Integers/solution.py
@@ -1,12 +1,16 @@
 class Solution:
     def getSum(self, a: int, b: int) -> int:
+        mask = 0xFFFFFFFF # Simulate a 32-bit signed integer (32 binary 1's), range: -2,147,483,648 to 2,147,483,647
+        max_int = 0x7FFFFFFF # 2,147,483,647
+
         while b != 0:
-            carry = (a&b) << 1
-            a ^= b
-            b = carry
-        return a
+            carry = (a & b) << 1
+            a = (a ^ b) & mask
+            b = carry & mask
+
+        return a if a <= max_int else ~(a ^ mask)
 
 if __name__ == "__main__":
     solution = Solution()
-    print(solution.getSum(a=1, b=1))
+    print(solution.getSum(a=-1, b=1))
     print(solution.getSum(a=4, b=7))
